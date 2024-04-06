67483a2effdb23b6766a0841d842b067a982bb2d
DorianLudm <122395205+DorianLudm@users.noreply.github.com>
2024-04-07 00:55:17 +0200

    Ajout d'un message lorsque l'utilisateur n'as pas d'objet

--- lib/pages/mesObjets.dart
+++ lib/pages/mesObjets.dart
@@ -58,8 +58,8 @@ class _MesObjets extends State<MesObjets> {
           Expanded(
             child: FutureBuilder<List<Objet>?>(
               future: MesObjetsProvider.db.getMesObjets()?.catchError((error) {
-              print('Error getting objects: $error');
-            }),
+                print('Error getting objects: $error');
+              }),
               builder: (BuildContext context, AsyncSnapshot<List<Objet>?> snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const SizedBox(
@@ -75,18 +75,24 @@ class _MesObjets extends State<MesObjets> {
                   if (snapshot.hasError) {
                     return Text('Error: ${snapshot.error}');
                   } else {
-                    print('Objects returned: ${snapshot.data}');
-                    return ListView.builder(
-                      itemCount: snapshot.data?.length ?? 0,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          child: ListTile(
-                            title: Text(snapshot.data![index].nomObjet),
-                            subtitle: Text(snapshot.data![index].descriptionObjet),
-                          ),
-                        );
-                      },
-                    );
+                    if (snapshot.data == null || snapshot.data!.isEmpty) {
+                      return const Center(
+                        child: Text('Ajouter un objet!'),
+                      );
+                    } else {
+                      print('Objects returned: ${snapshot.data}');
+                      return ListView.builder(
+                        itemCount: snapshot.data?.length ?? 0,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            child: ListTile(
+                              title: Text(snapshot.data![index].nomObjet),
+                              subtitle: Text(snapshot.data![index].descriptionObjet),
+                            ),
+                          );
+                        },
+                      );
+                    }
                   }
                 }
               },
